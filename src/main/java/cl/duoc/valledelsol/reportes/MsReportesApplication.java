108524cd3f4e79162c0ff83e6cee7ec8293c023a
JOOORMUNDGANDER <marcelo.sade.palma@gmail.com>
2026-04-30 03:37:16 -0400

    Version 1.0 Spring

---
+++ src/main/java/cl/duoc/valledelsol/reportes/MsReportesApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.valledelsol.reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReportesApplication.class, args);
+	}
+
+}
